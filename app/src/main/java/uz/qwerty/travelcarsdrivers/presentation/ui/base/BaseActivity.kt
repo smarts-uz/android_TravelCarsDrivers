97179d053292ad6b3b1c3004ca741bc5f72c9a46
AbdurashidovShahzod <shahzod9933@gmail.com>
2022-01-02 21:01:20 +0500

    Abdurashidov Sh 02.01.2022 21:02 😁 dependecy injection dagger hilt add* and base created BaseFragment.kt BaseVM.kt CourseRepository.kt CourseRepositoryImpl.kt CourseViewModel.kt RepositoryModule.kt App.kt Config.kt IsConnected.kt RetrofitModule.kt build.gradle Extensions.kt SuperAdapter.kt SuperListAdapter.kt ViewState.kt Default Changelist activity_booking.xml activity_booking.xml activity_calendar.xml activity_calendar.xml activity_login.xml activity_main.xml activity_main.xml activity_no_connection.xml activity_pin.xml activity_profile.xml activity_profile.xml activity_trips.xml activity_trips.xml AndroidManifest.xml App.kt Booking.kt BookingActiveAdapter.kt BookingActivity.kt BookingAdapter.kt BookingBannerAdapter.kt BookingBannerResponse.kt BookingDataFactory.kt BookingHystoryResponse.kt BookingRouteResponse.kt BookingShowResponse.kt BookingViewModel.kt CalendarActivity.kt compiler.xml Config.kt Config.kt content_booking.xml content_calendar.xml content_pin.xml content_profile.xml Counts.kt CountsResponse.kt FMessagingService.kt fragment_booking_done.xml gradle.xml LoginActivity.kt MainActivity.kt Meta.kt misc.xml NetworkState.kt NoConnectionActivity.kt PageKeyedBookingDataSource.kt PinActivity.kt ProfileActivity.kt ProfileUpdateResponse.kt Route.kt RouteAdapter.kt Status.kt TravelCarsApi.kt TravelCarsService.kt TripsActivity.kt User.kt UserResponse.kt vcs.xml build.gradle build.gradle compiler.xml gradle.xml gradle-wrapper.properties misc.xml BaseResponse.kt Config.kt CourseApi.kt CourseRepository.kt CourseRepositoryImpl.kt CourseViewModel.kt LoginActivity.kt MainActivity.kt misc.xml RepositoryModule.kt ResponseError.kt nbn.png RetrofitModule.kt WeatherActivity.kt WeatherActivityVM.kt WeatherApi.kt WeatherResponse.kt

--- app/src/main/java/uz/qwerty/travelcarsdrivers/presentation/ui/base/BaseActivity.kt
+++ app/src/main/java/uz/qwerty/travelcarsdrivers/presentation/ui/base/BaseActivity.kt
@@ -1,16 +1,25 @@
 package uz.qwerty.travelcarsdrivers.presentation.ui.base
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import uz.qwerty.travelcarsdrivers.presentation.ui.extensions.transparentStatusBar
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.viewbinding.ViewBinding
+import uz.qwerty.travelcarsdrivers.R
+import uz.qwerty.travelcarsdrivers.presentation.ui.extensions.showToast
 
 abstract class BaseActivity<T : ViewBinding> : AppCompatActivity() {
 
     private var _binding: ViewBinding? = null
     lateinit var binding: T
-
+    private var baseActivityStopped = false
+    private var baseMessageDialog: Dialog? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +35,51 @@ abstract class BaseActivity<T : ViewBinding> : AppCompatActivity() {
 
     abstract fun onCreated(savedInstanceState: Bundle?)
 
+    fun showErrorMessage(title: String = "", txt: String, callback: (() -> Unit)? = null) {
 
+        if (!baseActivityStopped) {
+            if (baseMessageDialog == null) {
 
+                loadDialog()
+                val statusImageview =
+                    baseMessageDialog?.findViewById<AppCompatImageView>(R.id.status_imageview)
+                statusImageview?.setImageResource(R.drawable.ic_warning)
+
+                val dialogTv = baseMessageDialog?.findViewById<TextView>(R.id.titletextview)
+                dialogTv?.text =
+                    if (title.isNotEmpty()) title else resources.getString(R.string.warning)
+                val errorDescription =
+                    baseMessageDialog?.findViewById<TextView>(R.id.error_description_textview)
+                errorDescription?.text = txt
+                val okButton = baseMessageDialog?.findViewById<Button>(R.id.ok_button)
+                okButton?.setOnClickListener {
+                    callback?.invoke()
+                    baseMessageDialog?.dismiss()
+                    baseMessageDialog = null
+                }
+                baseMessageDialog?.show()
+            }
+        } else {
+            showToast(txt)
+        }
+
+    }
+
+    private fun loadDialog() {
+        baseMessageDialog = Dialog(this)
+        baseMessageDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        baseMessageDialog?.setCancelable(false)
+        baseMessageDialog?.setContentView(R.layout.base_dialog_layout)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        baseActivityStopped = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        baseActivityStopped = false
+
+    }
 }
--- app/src/main/java/uz/qwerty/travelcarsdrivers/presentation/ui/base/BaseVM.kt
+++ app/src/main/java/uz/qwerty/travelcarsdrivers/presentation/ui/base/BaseVM.kt
@@ -1,5 +1,7 @@
 package uz.qwerty.travelcarsdrivers.presentation.ui.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
@@ -16,6 +18,18 @@ import kotlin.coroutines.EmptyCoroutineContext
  * shahzod9933@gmail.com
  */
 abstract class BaseVM : ViewModel() {
+
+    protected val _loading = MutableLiveData<Unit>()
+    val loading: LiveData<Unit> = _loading
+
+    protected val _empty = MutableLiveData<Unit>()
+    val empty: LiveData<Unit> = _empty
+
+    protected val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+//    protected val _success = MutableLiveData<T>()
+//    val success: LiveData<T> = _success
 
     protected fun launchViewModel(
         context: CoroutineContext = EmptyCoroutineContext,
