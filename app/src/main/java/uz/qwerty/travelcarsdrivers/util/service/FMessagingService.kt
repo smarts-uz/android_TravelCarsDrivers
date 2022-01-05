104cdaa73bda7989086f44271eec6d258231a532
AbdurashidovShahzod <shahzod9933@gmail.com>
2022-01-05 23:22:15 +0500

    Abdurashidov Sh 05.01.2022 23:23 😁 dependecy injection dagger hilt add* and base created BaseFragment.kt BaseVM.kt CourseRepository.kt CourseRepositoryImpl.kt CourseViewModel.kt RepositoryModule.kt App.kt Config.kt IsConnected.kt RetrofitModule.kt build.gradle Extensions.kt SuperAdapter.kt SuperListAdapter.kt ViewState.kt Default Changelist activity_booking.xml activity_booking.xml activity_calendar.xml activity_calendar.xml activity_login.xml activity_main.xml activity_main.xml activity_no_connection.xml activity_pin.xml activity_profile.xml activity_profile.xml activity_trips.xml activity_trips.xml AndroidManifest.xml App.kt Booking.kt BookingActiveAdapter.kt BookingActivity.kt BookingAdapter.kt BookingBannerAdapter.kt BookingBannerResponse.kt BookingDataFactory.kt BookingHystoryResponse.kt BookingRouteResponse.kt BookingShowResponse.kt BookingViewModel.kt CalendarActivity.kt compiler.xml Config.kt Config.kt content_booking.xml content_calendar.xml content_pin.xml content_profile.xml Counts.kt CountsResponse.kt FMessagingService.kt fragment_booking_done.xml gradle.xml LoginActivity.kt MainActivity.kt Meta.kt misc.xml NetworkState.kt NoConnectionActivity.kt PageKeyedBookingDataSource.kt PinActivity.kt ProfileActivity.kt ProfileUpdateResponse.kt Route.kt RouteAdapter.kt Status.kt TravelCarsApi.kt TravelCarsService.kt TripsActivity.kt User.kt UserResponse.kt vcs.xml build.gradle build.gradle compiler.xml gradle.xml gradle-wrapper.properties misc.xml BaseResponse.kt Config.kt CourseApi.kt CourseRepository.kt CourseRepositoryImpl.kt CourseViewModel.kt LoginActivity.kt MainActivity.kt misc.xml RepositoryModule.kt ResponseError.kt nbn.png RetrofitModule.kt WeatherActivity.kt WeatherActivityVM.kt WeatherApi.kt WeatherResponse.kt AndroidManifest.xml CalendarActivity.kt ic_add.xml MainActivity.kt misc.xml new_item_route_list.xml NewRouteAdapter.kt RouteAdapter.kt TravelCarsService.kt

--- app/src/main/java/uz/qwerty/travelcarsdrivers/util/service/FMessagingService.kt
+++ app/src/main/java/uz/qwerty/travelcarsdrivers/util/service/FMessagingService.kt
@@ -1,13 +1,74 @@
 package uz.qwerty.travelcarsdrivers.util.service
 
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import org.json.JSONObject
+import timber.log.Timber
 import uz.qwerty.travelcarsdrivers.R
+import uz.qwerty.travelcarsdrivers.presentation.app.App
+import uz.qwerty.travelcarsdrivers.presentation.ui.activity.MainActivity
 
 class FMessagingService : FirebaseMessagingService() {
+
+
+    fun generateMessage(title: String?, message: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+        val channelId = "Travel"
+        val channelName = "uz.qwerty.travelcarsdrivers.util.service"
+        var builder =
+            NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        applicationContext.resources,
+                        R.mipmap.ic_launcher
+                    )
+                )
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setColor(Color.parseColor("#394ac9"))
+                .setContentIntent(pendingIntent)
+        builder = builder.setContent(getRemoteView(title, message))
+    }
+
+    private fun getRemoteView(title: String?, message: String): RemoteViews? {
+        val remoteView =
+            RemoteViews("uz.qwerty.travelcarsdrivers.util.service", R.layout.notification)
+        remoteView.setTextViewText(R.id.txtTitle, title)
+        remoteView.setTextViewText(R.id.txtDesc, message)
+        remoteView.setImageViewResource(R.id.appLogo, R.drawable.launch_screen)
+        return remoteView
+    }
+
+
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+
+
         // ...
 
         // TODO(developer): Handle FCM messages here.
@@ -29,10 +90,84 @@ class FMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(s: String?) {
         super.onNewToken(s)
-        val sharedPref = this.getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE) ?: return
+        val sharedPref =
+            this.getSharedPreferences(getString(R.string.config), Context.MODE_PRIVATE) ?: return
         val editor = sharedPref.edit()
 
         editor.putString(getString(R.string.fcmToken), s)
         editor.apply()
     }
+
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun sendNotification(
+        title: String?,
+        text: String?,
+        action: String?,
+        sound: String?
+    ) {
+
+
+        //val fullLink = AUDIO_LINK + sound?.replace("_",".")
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra("TravelPush", action)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val channelId = "Travel"
+        val builder =
+            NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        applicationContext.resources,
+                        R.mipmap.ic_launcher
+                    )
+                )
+                .setContentTitle(title)
+                .setContentText(text)
+                .setAutoCancel(true)
+                .setColor(Color.parseColor("#394ac9"))
+                .setContentIntent(pendingIntent)
+
+
+//        if (sound == null){
+//            builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+//        } else {
+//
+//        }
+        val manager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "travel_channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            channel.setSound(null, null)
+            manager.createNotificationChannel(channel)
+        }
+
+
+    }
+
+    private fun playSound(fullLink: String) {
+        try {
+            val ringtone: Ringtone =
+                RingtoneManager.getRingtone(applicationContext, Uri.parse(fullLink))
+            ringtone.audioAttributes =
+                AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setLegacyStreamType(5)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT).build()
+            ringtone.play()
+        } catch (e: java.lang.Exception) {
+            Log.d("DETROIT", "error play sound $e")
+        }
+    }
+
+
 }
